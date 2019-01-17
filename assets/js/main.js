001846498308f8e7cff6e403051dca4ad57dad27
Paulo Henrique <paulo.mendes00@hotmail.com>
2019-01-17 11:22:12 -0200

    Incluindo bibliotecas javascript e css.

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,11 +61,11 @@ jQuery(document).ready(function($) {
     );
 
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "dougs007");
+    GitHubCalendar("#github-graph", "paulophmp");
 
 
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "dougs007", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "paulophmp", selector: "#ghfeed" });
 
 
 });
